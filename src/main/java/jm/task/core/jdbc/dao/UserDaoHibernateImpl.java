a21b1ea134bb852386ae5f8ee0a1635c4a44718a
vladlove28 <sergcechisto@gmail.com>
2023-04-18 23:13:20 +0300

    pom fix

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
 
-    Session session;
+   private Session session;
 
 
     public UserDaoHibernateImpl() {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -17,7 +17,7 @@ public class Util {
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/baseForKata";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Iloveenglish28";
+    private static final String PASSWORD = "rootroot";
 
     public static Connection getConnection() {
         Connection connection = null;
